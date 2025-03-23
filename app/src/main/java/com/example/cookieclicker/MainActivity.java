53df7f6c7d201375edb0ac9fe32915094f52f12f
Kalyan-chan <ecat2ch@gmail.com>
2025-03-24 00:36:55 +0600

    Лабораторная работа 2. Обновлено.

--- app/src/main/java/com/example/cookieclicker/MainActivity.java
+++ app/src/main/java/com/example/cookieclicker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cookieclicker;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -16,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
     private long count = 0;
     private TextView text;
+    private static final int DONATE_REQUEST_CODE = 1;
+
+    private int pointsPerClick = 1;
+    boolean notifiedNovice = false;
+    boolean notifiedAmateur = false;
+    boolean notifiedProfessional = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +33,19 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("DefaultLocale")
     public void onClick(View view) {
-        count++;
+        count += pointsPerClick;
         String timesText = getTimesText(count);
         text.setText(String.format("У вас %d %s", count, timesText));
 
-        if (count == 10) {
-            Toast.makeText(this, "Уровень новичка", Toast.LENGTH_SHORT).show();
-        }
-        if (count == 20) {
-            Toast.makeText(this, "Уровень любителя", Toast.LENGTH_SHORT).show();
-        }
-        if (count == 30) {
-            Toast.makeText(this, "Уровень профессионала", Toast.LENGTH_SHORT).show();
+        if (count >= 80 && !notifiedProfessional) {
+            Toast.makeText(this, "80: Уровень профессионала", Toast.LENGTH_SHORT).show();
+            notifiedProfessional = true;
+        } else if (count >= 50 && !notifiedAmateur) {
+            Toast.makeText(this, "50: Уровень любителя", Toast.LENGTH_SHORT).show();
+            notifiedAmateur = true;
+        } else if (count >= 30 && !notifiedNovice) {
+            Toast.makeText(this, "30: Уровень новичка", Toast.LENGTH_SHORT).show();
+            notifiedNovice = true;
         }
     }
 
@@ -50,4 +58,33 @@ public class MainActivity extends AppCompatActivity {
             return "печенек";
         }
     }
+
+    public void onClickDonateActivity(View view) {
+        Intent intent = new Intent(this, DonateActivity.class);
+        startActivityForResult(intent, DONATE_REQUEST_CODE);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == DONATE_REQUEST_CODE && resultCode == RESULT_OK) {
+            Toast.makeText(this, "❤\uFE0F Спасибо за поддержку!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (requestCode == 2 && resultCode == RESULT_OK) {
+            count = data.getLongExtra("count", count);
+            pointsPerClick = data.getIntExtra("pointsPerClick", pointsPerClick);
+            text.setText(String.format("У вас %d %s", count, getTimesText(count)));
+        }
+    }
+
+    public void onClickShopActivity(View view) {
+        Intent intent = new Intent(this, ShopActivity.class);
+        intent.putExtra("count", count);
+        intent.putExtra("pointsPerClick", pointsPerClick);
+        startActivityForResult(intent, 2);
+    }
+
 }
